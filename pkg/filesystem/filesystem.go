28e63276770306cf673fbf01f2551b723de00f80
Nayan Das <dastms@gmail.com>
2023-10-31 23:32:34 +0530

    Add Fetch to PathReader

--- pkg/filesystem/filesystem.go
+++ pkg/filesystem/filesystem.go
@@ -12,10 +12,15 @@ import (
 // reading the file.
 type PathReader interface {
 
-	// This functions keeps on sending the file names in dir directory
+	// This function keeps on sending the file names in dir directory
 	// to a write only channel ch until it receives an item in done channel.
 	// It sends the error to the e channel.
 	FetchWithChannel(dir string, ch chan<- string, e chan<- error)
+
+	// This function walks down the dir path and populates paths string slice
+	// with the files in the directory and subdirectories. The paths argument
+	// should be non nil or else it would return error
+	Fetch(dir string) ([]string, error)
 }
 
 type BuiltInPathReader struct {
@@ -23,6 +28,29 @@ type BuiltInPathReader struct {
 	// This has been stubbed out to better write the test
 	// cases with mock
 	Walk func(string, filepath.WalkFunc) error
+}
+
+func (bipr *BuiltInPathReader) Fetch(dir string) (paths []string, err error) {
+
+	paths = make([]string, 0)
+
+	err = bipr.Walk(dir, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("Error while reading metadata of file %q: %v\n", path, err)
+			return err
+		}
+
+		// fmt.Println(info.Name())
+
+		if !info.IsDir() {
+			paths = append(paths, info.Name())
+
+		}
+
+		return nil
+	})
+
+	return paths, err
 }
 
 func (bipr *BuiltInPathReader) FetchWithChannel(dir string, ch chan<- string, e chan<- error) {
--- pkg/filesystem/filesystem_test.go
+++ pkg/filesystem/filesystem_test.go
@@ -5,6 +5,10 @@ import (
 	"testing"
 )
 
+// Need to mock the file path walker instead of using the
+// path/filepath Walk function to prevent directly reading directory
+// and better check edge cases
+
 // func MockFilePathWalker(dir string, fs filepath.WalkFunc) error {
 // 	dirs := []string{
 // 		"/topdir/subdir/file1.jpg",
@@ -16,29 +20,49 @@ import (
 //	        fs(dir, )
 //	    }
 //	}
+
 func TestBuiltInPathReader(t *testing.T) {
-	r := NewBuiltInPathReader()
+	t.Run("test Fetch", func(t *testing.T) {
+		r := NewBuiltInPathReader()
 
-	ch := make(chan string, 10)
-	errCh := make(chan error)
+		paths, err := r.Fetch("/Users/nayan/Sites/projects/experiments/go/geoexif/images")
 
-	go r.FetchWithChannel("/Users/nayan/Sites/projects/experiments/go/geoexif/images", ch, errCh)
-
-	fileCount := 0
-
-L:
-	for {
-		select {
-		case <-ch:
-			fileCount++
-		case e := <-errCh:
-			t.Errorf("Halted due to error: %v", e)
-			break L
+		if err != nil {
+			t.Errorf("Got error: %v", err)
 		}
-	}
 
-	if fileCount != 9 {
-		t.Errorf("File count should be %v, got %v", 9, fileCount)
-	}
+		if len(paths) != 9 {
+			t.Errorf("Length of the slice should b %v, got %v", 9, len(paths))
+		}
+	})
+
+	t.Run("test FetchWithChannel", func(t *testing.T) {
+		r := NewBuiltInPathReader()
+
+		ch := make(chan string, 10)
+		errCh := make(chan error)
+
+		go r.FetchWithChannel("/Users/nayan/Sites/projects/experiments/go/geoexif/images", ch, errCh)
+
+		fileCount := 0
+
+	L:
+		for {
+			select {
+			case <-ch:
+				fileCount++
+			case e, ok := <-errCh:
+				if ok {
+					t.Errorf("Halted due to error: %v", e)
+				}
+				break L
+			}
+		}
+
+		if fileCount != 9 {
+			t.Errorf("File count should be %v, got %v", 9, fileCount)
+		}
+
+	})
 
 }
